8937299913c52ab6f249f06bccb96a66ab9fb766
ItzDelay <05bryanchan05@gmail.com>
2025-11-11 11:56:52 +0700

    Update MainActivity.cs

--- NoteApp/Platforms/Android/MainActivity.cs
+++ NoteApp/Platforms/Android/MainActivity.cs
@@ -1,19 +1,17 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Plugin.Fingerprint; // This line is still correct
+using Plugin.Fingerprint;
 
 namespace NoteApp;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    // THIS IS THE NEW, CORRECT OnCreate METHOD
     protected override void OnCreate(Bundle savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
-
-        // This is the correct way to initialize the plugin for MAUI
         CrossFingerprint.SetCurrentActivityResolver(() => this);
     }
+
 }
