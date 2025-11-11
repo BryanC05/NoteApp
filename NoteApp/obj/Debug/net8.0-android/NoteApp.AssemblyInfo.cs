fc2a4b5970ec06330c4dea501d2d91894c382652
ItzDelay <05bryanchan05@gmail.com>
2025-11-11 11:46:59 +0700

    Initial Publish

--- NoteApp/obj/Debug/net8.0-android/NoteApp.AssemblyInfo.cs
+++ NoteApp/obj/Debug/net8.0-android/NoteApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b23c533b70042f11d81c5077f9605ad4f6a5930b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
